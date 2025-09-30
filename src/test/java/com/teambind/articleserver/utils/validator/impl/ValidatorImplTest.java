2f79f1c50930b53c3eddd0b5c3b811fba181445c
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-30 19:57:24 +0900

    [TEST] ValidatorImpl 테스트 코드 추가 및 Readme 요구사항 업데이트 src/test/java/com/teambind/articleserver/utils/validator/impl/ValidatorImplTest.java: 입력값 예외 검증을 위한 테스트 메서드 추가 Readme.md: 요구사항 완료 항목 업데이트 및 테스트 이미지 추가 docs/images/Validator Test.png: Validator 테스트 이미지 추가
    참고: 데이터 매핑 검증 로직과 테스트 케이스 확장 필요
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

---
+++ src/test/java/com/teambind/articleserver/utils/validator/impl/ValidatorImplTest.java
@@ -0,0 +1,91 @@
+package com.teambind.articleserver.utils.validator.impl;
+
+import com.teambind.articleserver.exceptions.CustomException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ValidatorImplTest {
+	
+	@Test
+	@DisplayName("파마미터로 받은 보드ID 가 null 이면 예외를 던진다.")
+	void boardIdValidator1() {
+		ValidatorImpl validator = new ValidatorImpl();
+		
+		assertThrows(CustomException.class, () -> {
+			validator.boardIdValidator(null);
+		});
+	}
+	
+	@Test
+	@DisplayName("파마미터로 받은 보드ID 가 제공하지 않는 ID 값을 갖는다")
+	void boardIdValidator2() {
+		ValidatorImpl validator = new ValidatorImpl();
+		
+		assertThrows(CustomException.class, () -> {
+			validator.boardIdValidator(-9999L);
+		});
+	}
+	
+	@Test
+	@DisplayName("파라미터로 받은 보드명이 null 이면 예외를 던진다.")
+	void boardNameValidator1() {
+		ValidatorImpl validator = new ValidatorImpl();
+		
+		assertThrows(CustomException.class, () -> {
+			validator.boardNameValidator(null);
+		});
+	}
+	
+	@Test
+	@DisplayName("파라미터로 받은 보드명이 제공하지 않는 보드명이면 예외를 던진다.")
+	void boardNameValidator2() {
+		ValidatorImpl validator = new ValidatorImpl();
+		
+		assertThrows(CustomException.class, () -> {
+			validator.boardNameValidator("ㅁㄴㅇㅁ재엊ㅁ야ㅗㅓㅁ지ㅏㅓㅇ로ㅜ마ㅓㄹ");
+		});
+	}
+	
+	
+	@Test
+	@DisplayName("파라미터로 받은 키워드ID 가 null 이면 예외를 던진다.")
+	void keywordIdValidator1() {
+		ValidatorImpl validator = new ValidatorImpl();
+		
+		assertThrows(CustomException.class, () -> {
+			validator.keywordIdValidator(null);
+		});
+	}
+	
+	@Test
+	@DisplayName("파라미터로 받은 키워드ID 가 제공하지 않는 ID 값을 갖는다")
+	void keywordIdValidator2() {
+		ValidatorImpl validator = new ValidatorImpl();
+		
+		assertThrows(CustomException.class, () -> {
+			validator.keywordIdValidator(-9999L);
+		});
+	}
+	
+	@Test
+	@DisplayName("파라미터로 받은 키워드명이 null 이면 예외를 던진다.")
+	void keywordNameValidator1() {
+		ValidatorImpl validator = new ValidatorImpl();
+		
+		assertThrows(CustomException.class, () -> {
+			validator.keywordNameValidator(null);
+		});
+	}
+	
+	@Test
+	@DisplayName("파라미터로 받은 키워드명이 제공하지 않는 키워드명이면 예외를 던진다.")
+	void keywordNameValidator2() {
+		ValidatorImpl validator = new ValidatorImpl();
+		
+		assertThrows(CustomException.class, () -> {
+			validator.keywordNameValidator("adhjdlkiawhjdlaiwjdlawij");
+		});
+	}
+}
