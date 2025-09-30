b881882e99cb934722ec784bccc160f740481c23
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-30 20:59:04 +0900

    [FEAT] Validator 개선 및 테스트 코드 업데이트
    src/main/java/com/teambind/articleserver/exceptions/ErrorCode.java: 새로운 에러 코드 'REQUIRED_FIELD_NOT_VALID' 추가
    src/main/java/com/teambind/articleserver/utils/validator/Validator.java: boardValidator, keywordValidator 메서드 추가로 Object 기반 입력값 검증 지원
    src/main/java/com/teambind/articleserver/utils/validator/impl/ValidatorImpl.java: boardValidator, keywordValidator 구현 및 기존 메서드 내부 로직 개선
    src/test/java/com/teambind/articleserver/utils/validator/impl/ValidatorImplTest.java: 유형별 boardValidator 및 keywordValidator 테스트 메서드 추가
    
    참고: 기존 테스트 코드 주석 처리(코드 리팩토링 이후 수정 필요)
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/articleserver/utils/validator/Validator.java
+++ src/main/java/com/teambind/articleserver/utils/validator/Validator.java
@@ -2,9 +2,6 @@ package com.teambind.articleserver.utils.validator;
 
 public interface Validator {
 	
-
-	
-	// TODO : Object -> isInstance 하면 Id value 한번에 합칠수 있음
 	
 	void boardValidator(Object board);
 	
