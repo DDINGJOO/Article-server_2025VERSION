78a578e87786b70fb3bd15a90a19b101c82e31b3
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-30 21:35:04 +0900

    [FEAT] 기사 차단 상태 예외 처리 로직 추가
    src/main/java/com/teambind/articleserver/exceptions/ErrorCode.java: 새로운 에러 코드 'ARTICLE_IS_BLOCKED' 추가
    src/main/java/com/teambind/articleserver/service/crud/impl/ArticleReadService.java: BLOCKED 및 DELETED 상태에 대한 예외 처리 로직 추가
    
    참고: fetchArticleById 호출부에서 상태 예외 처리 확인 필요
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/articleserver/exceptions/ErrorCode.java
+++ src/main/java/com/teambind/articleserver/exceptions/ErrorCode.java
@@ -3,7 +3,6 @@ package com.teambind.articleserver.exceptions;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-
 @Getter
 public enum ErrorCode {
 	BOARD_ID_NULL("REQ_ERR_001", "BOARD_ID_IS_NULL", HttpStatus.BAD_REQUEST),
@@ -13,9 +12,9 @@ public enum ErrorCode {
 	KEYWORD_NAME_NULL("REQ_ERR_005", "KEYWORD_NAME_IS_NULL", HttpStatus.BAD_REQUEST),
 	ARTICLE_NOT_FOUND("ART_ERR_001", "ARTICLE_NOT_FOUND", HttpStatus.NOT_FOUND),
 	REQUIRED_FIELD_NOT_VALID("REQ_ERR_006", "REQUIRED_FIELD_NOT_VALIED", HttpStatus.BAD_REQUEST),
-	;
-	
-	
+  ARTICLE_IS_BLOCKED("ART_ERR_002", "ARTICLE_IS_BLOCKED", HttpStatus.BAD_REQUEST),
+  ;
+
 	private final String errCode;
 	private final String message;
 	private final HttpStatus status;
--- src/main/java/com/teambind/articleserver/service/crud/impl/ArticleReadService.java
+++ src/main/java/com/teambind/articleserver/service/crud/impl/ArticleReadService.java
@@ -1,6 +1,7 @@
 package com.teambind.articleserver.service.crud.impl;
 
 import com.teambind.articleserver.entity.Article;
+import com.teambind.articleserver.entity.enums.Status;
 import com.teambind.articleserver.exceptions.CustomException;
 import com.teambind.articleserver.exceptions.ErrorCode;
 import com.teambind.articleserver.repository.ArticleRepository;
@@ -15,8 +16,16 @@ public class ArticleReadService {
   private final ArticleRepository articleRepository;
 
   Article fetchArticleById(String articleId) {
-    return articleRepository
-        .findById(articleId)
-        .orElseThrow(() -> new CustomException(ErrorCode.ARTICLE_NOT_FOUND));
+    Article article =
+        articleRepository
+            .findById(articleId)
+            .orElseThrow(() -> new CustomException(ErrorCode.ARTICLE_NOT_FOUND));
+    if (article.getStatus() == Status.BLOCKED) {
+      throw new CustomException(ErrorCode.ARTICLE_IS_BLOCKED);
+    }
+    if (article.getStatus() == Status.DELETED) {
+      throw new CustomException(ErrorCode.ARTICLE_NOT_FOUND);
+    }
+    return article;
   }
 }
