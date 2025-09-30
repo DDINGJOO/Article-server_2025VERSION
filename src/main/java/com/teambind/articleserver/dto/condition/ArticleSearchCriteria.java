56d9705ac9767552d0a15c70d9d2a9c242e40bd7
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-30 20:15:35 +0900

    [FEAT] Article 관련 DTO, 서비스 인터페이스 및 구현체 추가
    src/main/java/com/teambind/articleserver/dto/response/ArticleResponse.java: 게시글 조회용 응답 DTO 추가
    src/main/java/com/teambind/articleserver/dto/condition/ArticleSearchCriteria.java: 게시글 검색 조건 DTO 추가
    src/main/java/com/teambind/articleserver/service/ArticleService.java: 게시글 생성, 수정, 삭제, 조회를 위한 서비스 인터페이스 추가
    src/main/java/com/teambind/articleserver/service/impl/ArticleCreateService.java: 게시글 생성 관련 기본 구현체 추가
    src/main/java/com/teambind/articleserver/dto/request/ArticleCreateRequest.java: 게시글 생성 요청용 DTO 추가
    src/main/java/com/teambind/articleserver/utils/validator/Validator.java: 입력 검증 관련 TODO 코멘트 추가
    
    참고: 서비스 구현 로직 자세화 및 관련 테스트 코드 필요
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

---
+++ src/main/java/com/teambind/articleserver/dto/condition/ArticleSearchCriteria.java
@@ -0,0 +1,22 @@
+package com.teambind.articleserver.dto.condition;
+
+import com.teambind.articleserver.entity.enums.Status;
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ArticleSearchCriteria {
+	
+	// 키워드 아이디 -> 키워드 아이디를 갖고있는 게시물(by Keyword Mapping Table)
+	private List<Integer> keywordIds;
+	private Long boardId;
+	private String title;
+	private String content;
+	private String writerId;
+	private Status status;
+}
