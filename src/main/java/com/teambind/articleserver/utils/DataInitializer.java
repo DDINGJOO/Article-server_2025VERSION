95b49593d1917382e7f6cdc5ff768a17bacfafcb
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-30 19:24:49 +0900

    [FEAT] DataInitializer에 키워드 초기화 로직 추가
    src/main/java/com/teambind/articleserver/utils/DataInitializer.java: `PostConstruct`를 활용해 키워드 초기화 로직 및 로깅 추가
    
    참고: 키워드 데이터 추가 시 동기화 확인 필요
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/articleserver/utils/DataInitializer.java
+++ src/main/java/com/teambind/articleserver/utils/DataInitializer.java
@@ -1,10 +1,31 @@
 package com.teambind.articleserver.utils;
 
 
+import com.teambind.articleserver.repository.KeywordRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DataInitializer {
+	public static Map<Long, String> keywordMap = new HashMap<>();
+	private final KeywordRepository keywordRepository;
+	
+	@PostConstruct
+	public void init() {
+		keywordRepository.findAll().forEach(keyword -> {
+			keywordMap.put(keyword.getId(), keyword.getKeyword());
+		});
+		
+		log.info("keywordMap size : {}", keywordMap.size());
+		keywordMap.forEach((k, v) -> {
+			log.info("keywordMap keyword : {}, id : {}", v, k);
+		});
+	}
 }
