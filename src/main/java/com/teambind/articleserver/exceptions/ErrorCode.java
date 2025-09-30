51c06da72f0948777d4724efc04a6c56bdf19715
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-30 19:57:03 +0900

    [FEAT] 예외 관리용 CustomException 및 ErrorCode 추가
    src/main/java/com/teambind/articleserver/exceptions/CustomException.java: 커스텀 예외 클래스 추가 (ErrorCode와 연동)
    src/main/java/com/teambind/articleserver/exceptions/ErrorCode.java: 공통 에러 코드 정의 및 관련 상태 정보 추가
    
    참고: 예외 메시지 및 상태 코드 사용 검증 필요
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

---
+++ src/main/java/com/teambind/articleserver/exceptions/ErrorCode.java
@@ -0,0 +1,37 @@
+package com.teambind.articleserver.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+public enum ErrorCode {
+	BOARD_ID_NULL("REQ_ERR_001", "BOARD_ID_IS_NULL", HttpStatus.BAD_REQUEST),
+	REQUIRED_FIELD_NULL("REQ_ERR_002", "REQUIRED_FIELD_IS_NULL", HttpStatus.BAD_REQUEST),
+	BOARD_NAME_NULL("REQ_ERR_003", "BOARD_NAME_IS_NULL", HttpStatus.BAD_REQUEST),
+	KEYWORD_ID_NULL("REQ_ERR_004", "KEYWORD_ID_IS_NULL", HttpStatus.BAD_REQUEST),
+	KEYWORD_NAME_NULL("REQ_ERR_005", "KEYWORD_NAME_IS_NULL", HttpStatus.BAD_REQUEST),
+	;
+	
+	
+	private final String errCode;
+	private final String message;
+	private final HttpStatus status;
+	
+	ErrorCode(String errCode, String message, HttpStatus status) {
+		
+		this.status = status;
+		this.errCode = errCode;
+		this.message = message;
+	}
+	
+	@Override
+	public String toString() {
+		return "ErrorCode{" +
+				" status='" + status + '\'' +
+				"errCode='" + errCode + '\'' +
+				", message='" + message + '\'' +
+				'}';
+	}
+	
+}
