9dec72a3a669728fadf096a38e618f72a9cd0e9a
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-01 09:04:32 +0900

    [FEAT] 기사 조회 엔드포인트 추가 및 DTO 컨버터 확장
    src/main/java/com/teambind/articleserver/controller/ArticleController.java: 기사 조회 API("/api/articles/{articleId}") 추가
    src/main/java/com/teambind/articleserver/service/crud/impl/ArticleReadService.java: fetchArticleById 메서드 접근 제한자 변경(public)
    src/main/java/com/teambind/articleserver/dto/response/ArticleResponse.java: Article 엔티티 변환 메서드(fromEntity)와 이미지, 키워드 처리 로직 추가
    
    참고: ArticleResponse 관련 테스트 케이스 추가 필요
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/articleserver/controller/ArticleController.java
+++ src/main/java/com/teambind/articleserver/controller/ArticleController.java
@@ -1,19 +1,18 @@
 package com.teambind.articleserver.controller;
 
 import com.teambind.articleserver.dto.request.ArticleCreateRequest;
+import com.teambind.articleserver.dto.response.ArticleResponse;
 import com.teambind.articleserver.entity.Article;
 import com.teambind.articleserver.entity.Board;
 import com.teambind.articleserver.entity.Keyword;
 import com.teambind.articleserver.service.crud.impl.ArticleCreateService;
+import com.teambind.articleserver.service.crud.impl.ArticleReadService;
 import com.teambind.articleserver.utils.convertor.Convertor;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/articles")
@@ -21,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ArticleController {
   private final ArticleCreateService articleCreateService;
+  private final ArticleReadService articleReadService;
   private final Convertor convertor;
 
   @PostMapping()
@@ -35,5 +35,13 @@ public class ArticleController {
     log.info("게시글이 성공적으로 저장되었습니다. article id : {}", article.getId());
 
     return ResponseEntity.ok(article.getId());
+  }
+
+  @GetMapping("/{articleId}")
+  public ResponseEntity<ArticleResponse> fetchArticle(
+      @PathVariable(name = "articleId") String articleId) {
+    Article article = articleReadService.fetchArticleById(articleId);
+
+    return ResponseEntity.ok(ArticleResponse.fromEntity(article));
   }
 }
