02a853e40bf3c2822cd11e5713d56ca5256d5943
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-13 13:33:43 +0900

    [FEAT] Bulk Article 조회 응답 처리 추가
    src/main/java/com/teambind/articleserver/controller/BulkController.java: Bulk Article 조회 시 ResponseEntity와 간단한 List 응답 처리 추가
    
    참고: 실제 데이터 응답 로직 구현 필요
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/articleserver/controller/BulkController.java
+++ src/main/java/com/teambind/articleserver/controller/BulkController.java
@@ -1,6 +1,9 @@
 package com.teambind.articleserver.controller;
 
+import com.teambind.articleserver.dto.response.ArticleSimpleResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BulkController {
 
   @GetMapping("/articles")
-  public void bulkArticles() {}
+  public ResponseEntity<List<ArticleSimpleResponse>> bulkArticles( ) {
+	  return ResponseEntity.ok(List.of());
+  }
 }
